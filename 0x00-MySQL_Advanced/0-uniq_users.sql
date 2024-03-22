f6c2628a693263344c3fd6f51c562013df746744
Albert <linkipskirui@gmail.com>
2024-03-22 02:55:22 +0000

    MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Task : 0. Script that creates a table users
+CREATE TABLE If NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
+);
